eaf43286ff65719f08435f2521a79311448e3490
Akira SUENAMI <a.suenami@gmail.com>
2014-03-28 14:56:19 +0900

    Initialize rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DciSample::Application.config.secret_key_base = 'c0416d0b89e6f2ef4be234318eef11e7d07de5b8b1e88d3fe300428d359a7571deca0e41737ed379c81f867156037a424d3f5f4028b77075f126657d9db36808'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DciSample::Application.config.session_store :cookie_store, key: '_dci_sample_session'
